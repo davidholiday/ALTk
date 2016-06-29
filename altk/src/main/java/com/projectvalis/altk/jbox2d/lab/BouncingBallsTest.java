76562cc403a62f0990d55135175ba9722e50442f
dholiday <davidholiday@hush.com>
2016-06-28 22:24:57 -0700

    adds yet untested utility methods to convert pixelworld to box2d world coordinates and back again

--- altk/src/main/java/com/projectvalis/altk/jbox2d/lab/BouncingBallsTest.java
+++ altk/src/main/java/com/projectvalis/altk/jbox2d/lab/BouncingBallsTest.java
@@ -13,12 +13,12 @@ public class BouncingBallsTest extends TestbedTest {
 	@Override
 	public void initTest(boolean deserialized) {
 		
-		// balls 
-		//
 		Vec2 gravityVector = new Vec2(0, -10);
-		//this.getWorld().setGravity(new Vec2());
 		this.getWorld().setGravity(gravityVector);
 		
+		
+		// balls 
+		//
 		BodyDef circleBodyDef = new BodyDef();
 		Vec2 screenCenterVector = new Vec2(10, 10);
 		// TODO create coordPixesToWorld analog (or just import it from box2d
@@ -31,20 +31,59 @@ public class BouncingBallsTest extends TestbedTest {
 		
 		Body circleBody = this.getWorld().createBody(circleBodyDef);
         circleBody.createFixture(circleShape, 100);		
+        circleBody.getFixtureList().setDensity(1);
         circleBody.getFixtureList().setRestitution(0.6f);
+        circleBody.getFixtureList().setFriction(0.3f);
         
         
         // the ground
         //
         BodyDef groundBodyDef = new BodyDef();
-        Vec2 groundBodyPositionVector = new Vec2(0, -10);
+        Vec2 groundBodyPositionVector = new Vec2(0, -11);
         groundBodyDef.setPosition(groundBodyPositionVector);;
   
         PolygonShape groundShape = new PolygonShape();
-        groundShape.setAsBox(50, 10);
+        groundShape.setAsBox(60, 0);
         
         Body groundBody = this.getWorld().createBody(groundBodyDef);
         groundBody.createFixture(groundShape, 0);
+        
+        
+        // walls
+        //
+        BodyDef leftWallBodyDef = new BodyDef();
+        Vec2 leftWallBodyPositionVector = new Vec2(-31, 0);
+        leftWallBodyDef.setPosition(leftWallBodyPositionVector);;
+  
+        PolygonShape leftWallShape = new PolygonShape();
+        leftWallShape.setAsBox(0, 60);
+        
+        Body leftWallBody = this.getWorld().createBody(leftWallBodyDef);
+        leftWallBody.createFixture(leftWallShape, 0);
+        
+        
+        BodyDef rightWallBodyDef = new BodyDef();
+        Vec2 rightWallBodyPositionVector = new Vec2(31, 0);
+        rightWallBodyDef.setPosition(rightWallBodyPositionVector);;
+  
+        PolygonShape rightWallShape = new PolygonShape();
+        rightWallShape.setAsBox(0, 60);
+        
+        Body rightWallBody = this.getWorld().createBody(rightWallBodyDef);
+        rightWallBody.createFixture(rightWallShape, 0);
+        
+        
+        // ceiling
+        //
+        BodyDef ceilingBodyDef = new BodyDef();
+        Vec2 ceilingBodyPositionVector = new Vec2(0, 51);
+        ceilingBodyDef.setPosition(ceilingBodyPositionVector);;
+  
+        PolygonShape ceilingShape = new PolygonShape();
+        ceilingShape.setAsBox(60, 0);
+        
+        Body ceilingBody = this.getWorld().createBody(ceilingBodyDef);
+        ceilingBody.createFixture(ceilingShape, 0);
         
 	}
 
---
+++ altk/src/main/java/com/projectvalis/altk/util/Jbox2dUtils.java
@@ -0,0 +1,70 @@
+package com.projectvalis.altk.util;
+
+import org.jbox2d.common.Vec2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * two utiltities to convert pixel-world to box2d-world coordinates and back.
+ * see the below for more details
+ * 
+ * http://www.box2d.org/forum/viewtopic.php?f=3&t=8198 
+ * http://box2d.org/2011/12/pixels/
+ * 
+ * TODO test them
+ * 
+ * @author snerd
+ *
+ */
+public class Jbox2dUtils {
+
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(Jbox2dUtils.class.getName());
+	
+	public static final int NUM_PIXELS_TO_METER = 50;
+	
+	
+	/**
+	 * 
+	 * @param box2dCoordinate
+	 * @param screenCenterInPixel
+	 * @return
+	 */
+	public static Vec2 box2dToPixelCoordinate(Vec2 box2dCoordinate,
+			                                  Vec2 screenCenterInPixel) {
+		
+		float offsetX = screenCenterInPixel.x / NUM_PIXELS_TO_METER;
+		float offsetY = screenCenterInPixel.y / NUM_PIXELS_TO_METER;
+		
+		Vec2 pixelCoordinate = new Vec2();
+		pixelCoordinate.x = box2dCoordinate.x + offsetX;
+		pixelCoordinate.y = -box2dCoordinate.y + offsetY;
+		
+		return pixelCoordinate;
+	}
+	
+	
+
+	/**
+	 * 
+	 * @param pixelCoordinate
+	 * @param screenCenterInPixel
+	 * @return
+	 */
+	public static Vec2 pixelToBox2dCoordinate(Vec2 pixelCoordinate,
+			                                  Vec2 screenCenterInPixel) {
+		
+		float offsetX = screenCenterInPixel.x;
+		float offsetY = screenCenterInPixel.y;
+		
+		Vec2 box2dCoordinate = new Vec2();
+		box2dCoordinate.x = pixelCoordinate.x - offsetX / NUM_PIXELS_TO_METER;
+		box2dCoordinate.y = -pixelCoordinate.y + offsetY / NUM_PIXELS_TO_METER;
+		
+		return box2dCoordinate;
+	}
+	
+	
+}
